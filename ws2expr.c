c50604084d5c24c76cafc4d5a4905c2cc19933a4
s6704062611255 <s6704062611255@email.kmutnb.ac.th>
2025-12-23 20:55:52 +0700

    Add files via upload

---
+++ ws2expr.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    double a,b,c,d;
+    //a=1.0 , b=1.0 , c=5.0, d=1.0;
+    //a=9.0 , b=2.0 , c=0.0, d=1.0;
+    a=0.0 , b=3.0 , c=3.0, d=3.0;
+
+    printf("A = %.2lf\n",(3.0*a/5.0 + (1.0/b)));
+    printf("B = %.2lf\n", ((3.0*a+5.0*b))/2.0+(-2.0));
+    printf("C = %.2lf\n",(2.0/7.0)*((pow(4.0,3.0+-2)) - 5.0*-2));
+    printf("D = %.2lf\n",sqrt((2.0+8.0*b)/a));
+    printf("E = %.2lf\n",cbrt((2.0*2.0-4.0*1.0)));
+}
